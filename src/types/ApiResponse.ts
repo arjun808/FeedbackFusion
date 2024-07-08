7329dc63c1916d093baa1e770c8241f1449f086c
Arjun patidar <arjunpatidarprofessional@gmail.com>
2024-07-08 10:13:10 +0530

    Initial commit

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,9 @@
+import { Message } from "@/models/UserModel";
+
+export interface ApiResponse{
+    success:boolean;
+    message:string;
+    isAcceptingMessages?:boolean;
+    messages?:Array<Message>;
+    verifyCode?:number
+}
